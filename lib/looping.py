c24743784eebfaa4c939852bbc34e3b85e127ac4
peterkomo <mwangipeterkomo@gmail.com>
2023-08-16 12:01:53 +0300

    done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,46 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown=10
+    while countdown >=1:
+         print (countdown)
+         countdown -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+
+def square_integers(numbers):
+    squared_list = [num ** 2 for num in numbers]
+    return squared_list
+
+input_list = [1, 2, 3, 4, 5]
+result = square_integers(input_list)
+print(result)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+def reverse_string(input_str):
+    reversed_str = ""
+    for char in input_str:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+print(reverse_string("hello"))  
+print(reverse_string("world")) 
+
+
+def square_integers(int_list):
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
+
+print(square_integers([1, 2, 3, 4, 5]))  
+print(square_integers([0, -1, 2, -3]))   
